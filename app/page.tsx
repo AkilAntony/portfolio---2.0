d691a7b880f82a344ae323ee0844585d1874d2c3
Akil <akhilal778@gmail.com>
2026-03-31 21:33:24 +0530

    feat : add loader UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Loader from "@/components/common/Loader/Loader";
 import PortfolioMain from "@/components/porfolio/PortfolioMain";
 import { Suspense } from "react";
 
 export default function Portfolio() {
- 
-
   return (
-    <Suspense fallback = {<div>Loading..</div>}>
+    <Suspense
+      fallback={
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          <Loader />
+        </div>
+      }
+    >
       <PortfolioMain />
     </Suspense>
   );
